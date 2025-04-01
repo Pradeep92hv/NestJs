b288db5d766b7e36e7a16f6520d001dbb35d8cde
Pradeep  H V <hvpradeep92@gmail.com>
2025-04-01 20:38:19 +0530

    basic post api,  26:11 min

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,5 +2,11 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AuthService{
-    test(){}
+    signup(){
+         return 'I am signed up'
+    }
+
+    signin(){
+        return {msg: 'I am signed In'};
+    }
 }
