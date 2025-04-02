2e8c209e8d293fbf0f953ba43fab7e99a3a2fb9f
Pradeep  H V <hvpradeep92@gmail.com>
2025-04-02 17:31:28 +0530

    congig module . for env variable  1:33 hr

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal:true       // to make congig module visible for all module, we using in prisma service
+  }), 
+    AuthModule, UserModule, BookmarkModule, PrismaModule],
 
 })
 export class AppModule {}
