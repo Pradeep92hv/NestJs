9e3f6cee80594bbf254cc0d970887a993192f961
Pradeep  H V <hvpradeep92@gmail.com>
2025-04-03 21:33:39 +0530

    AuthGaurd in sepeeate folder

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('user')
 export class UserController {
 
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(JwtGuard)
     @Get('me')
-    getme(){
-        return "me"
+    getme(@Req() req:Request & { user :any}){
+        return req.user;
     }
 }
